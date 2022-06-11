805075c41d8ff6f0db6025264eeac9feea61dd3b
Mariah Hines <90354081+MHines0512@users.noreply.github.com>
2022-06-11 18:28:05 -0400

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,71 +1,75 @@
 function Withdraw(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [withdrawal, setWithdrawal]   = React.useState('');
+   const [balance, setBalance]   = React.useState(100);
+   const [disabled, setDisabled] = React.useState(true);
+  const ctx = React.useContext(UserContext); 
+
+  const validate = amount => {
+    if(!amount) {
+      setStatus('Error: Please enter a value');
+      return false;
+    }
+    if(!Number(amount)) {
+      //(amount === NaN) doesnt work
+      setStatus('Error: Please enter a valid number');
+      return false;
+    }
+    if(amount < 0) {
+      setStatus("Error: Cannot withdraw a negative amount");
+      return false;
+    }
+    if (balance - amount < 0) {
+      setStatus('Error: Overdrafting account')
+      return false;
+    }
+    return true;
+  }
+
+  const withdrawMoney = amount => {
+    if (!validate(amount)) return;
+    setBalance(Number(balance) - Number(amount));
+    setShow(false);
+    setStatus('');
+    ctx.users.push(Number(balance) - Number(amount));
+  }
+
+  function clearForm(){
+    setWithdrawal('');
+    setShow(true);
+  }
+
+  React.useEffect(() => {
+		if (!withdrawal) {
+			setDisabled(true);
+		}
+		else {
+			setDisabled(false);
+		}
+	}, [withdrawal]);
 
   return (
     <Card
-      bgcolor="success"
+      bgcolor="danger"
       header="Withdraw"
       status={status}
-      body={show ? 
-        <WithdrawForm setShow={setShow} setStatus={setStatus}/> :
-        <WithdrawMsg setShow={setShow}/>}
+      body={show ? (  
+              <>
+              <h5>Balance: ${balance}</h5>
+              <br/>
+              Withdrawal Amount<br/>
+              <input type="withdraw" className="form-control" id="withdraw" placeholder="Withdraw Amount $" value={withdrawal} onChange={e => setWithdrawal(e.currentTarget.value)}/><br/>
+              <button type="submit" className="btn btn-light" onClick={() => withdrawMoney(withdrawal)} disabled={disabled}>Withdraw</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Make another withdrawal?</button>
+              </>
+            )}
     />
   )
 }
 
-function WithdrawMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => props.setShow(true)}>
-        Withdraw again
-    </button>
-  </>);
-}
 
-function WithdrawForm(props){
-  const [email, setEmail]   = React.useState('');
-  const [amount, setAmount] = React.useState('');
-  const ctx = React.useContext(UserContext);  
-
-  function handle(){
-    console.log(email,amount);
-    const user = ctx.users.find((user) => user.email == email);
-    if (!user) {
-      props.setStatus('fail!')      
-      return;      
-    }
-
-    user.balance = user.balance - Number(amount);
-    console.log(user);
-    props.setStatus('');      
-    props.setShow(false);
-  }
-
-
-  return(<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Amount<br/>
-    <input type="number" 
-      className="form-control" 
-      placeholder="Enter amount" 
-      value={amount} 
-      onChange={e => setAmount(e.currentTarget.value)}/><br/>
-
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={handle}>
-        Withdraw
-    </button>
-
-  </>);
-}
